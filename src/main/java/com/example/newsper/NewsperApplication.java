281f264dfbc8f512ce52ff631d31d4d3b78a5acf
ine9141 <ine9141@gmail.com>
2023-07-17 15:00:22 +0900

    init

---
+++ src/main/java/com/example/newsper/NewsperApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newsper/NewsperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newsper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
