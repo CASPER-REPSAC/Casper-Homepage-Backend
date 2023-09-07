801f968350d31a2dd82d153703ed7504c5e187a0
ine9141 <ine9141@gmail.com>
2023-09-07 15:56:19 +0900

    build

--- src/main/java/com/example/newsper/NewsperApplication.java
+++ src/main/java/com/example/newsper/NewsperApplication.java
@@ -3,12 +3,19 @@ package com.example.newsper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class NewsperApplication {
+public class NewsperApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsperApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(NewsperApplication.class);
 	}
 
 }
