f3728190df65da9179e51c0d9a6443b1a80f074e
ine9141 <ine9141@gmail.com>
2023-09-08 13:51:44 +0900

    api v1.3 빌드

--- src/main/java/com/example/newsper/NewsperApplication.java
+++ src/main/java/com/example/newsper/NewsperApplication.java
@@ -3,13 +3,12 @@ package com.example.newsper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class NewsperApplication{
+public class NewsperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsperApplication.class, args);
 	}
+
 }
