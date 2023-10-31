3736315be4c94d9c22265cea3e171f39b9f49d7f
ine9141 <ine9141@gmail.com>
2023-10-31 18:12:02 +0900

    api v1.3.2 api 명세 수정

--- src/main/java/com/example/newsper/entity/ArticleEntity.java
+++ src/main/java/com/example/newsper/entity/ArticleEntity.java
@@ -5,7 +5,7 @@ import lombok.*;
 
 import java.util.Date;
 
-@Entity
+@Entity(name="articleEntity")
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
--- src/main/java/com/example/newsper/entity/UserEntity.java
+++ src/main/java/com/example/newsper/entity/UserEntity.java
@@ -5,7 +5,7 @@ import lombok.*;
 
 import java.util.Date;
 
-@Entity
+@Entity(name="userEntity")
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
