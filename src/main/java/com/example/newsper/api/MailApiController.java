bba5ff9ca31a7671a77ad799f660d0e2862bfcee
ine9141 <ine9141@gmail.com>
2024-04-30 16:55:13 +0900

    .

--- src/main/java/com/example/newsper/api/MailApiController.java
+++ src/main/java/com/example/newsper/api/MailApiController.java
@@ -1,6 +1,8 @@
 package com.example.newsper.api;
 
 import com.example.newsper.service.MailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
@@ -21,14 +23,8 @@ public class MailApiController {
     @Autowired
     private MailService mailService;
 
-    // 인증 이메일 전송
-    @PostMapping("/verify")
-    public Boolean sendEmailAndCode(@RequestParam(value = "email") String email_addr, @RequestParam(value = "code") String code) {
-        return mailService.verifyEmailCode(email_addr, code);
-    }
-
-    // 인증번호 일치여부 확인
-    @GetMapping("/send")
+    @Operation(summary= "인증 메일 전송", description= "인증 메일을 전송합니다.")
+    @PostMapping("/send")
     public ResponseEntity<String> sendEmailPath(@RequestParam(value = "email") String email_addr) {
         mailService.sendEmail(email_addr);
         return ResponseEntity.status(HttpStatus.OK).build();
