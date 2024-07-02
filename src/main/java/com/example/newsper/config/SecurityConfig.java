2834674856cdf024b602c08b98a52b7c3ca00a66
ine <ine9141@gmail.com>
2024-07-02 19:24:18 +0900

    .

--- src/main/java/com/example/newsper/config/SecurityConfig.java
+++ src/main/java/com/example/newsper/config/SecurityConfig.java
@@ -23,7 +23,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
--- src/main/java/com/example/newsper/config/WebMvcConfig.java
+++ src/main/java/com/example/newsper/config/WebMvcConfig.java
@@ -17,10 +17,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("https://casper.or.kr")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**")  // 모든 경로에 대해 허용
+                .allowedOrigins("*")  // 모든 도메인 허용
+                .allowedMethods("*")  // 모든 HTTP 메서드 허용
+                .allowedHeaders("*")  // 모든 헤더 허용
+                .allowCredentials(false);
     }
 }
