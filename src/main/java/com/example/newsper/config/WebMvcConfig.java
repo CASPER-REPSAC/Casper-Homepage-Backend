67c9e2b8996d9a9c0f95d7995ddfa226961a024a
ine <ine9141@gmail.com>
2024-07-04 20:40:45 +0900

    .

--- src/main/java/com/example/newsper/config/WebMvcConfig.java
+++ src/main/java/com/example/newsper/config/WebMvcConfig.java
@@ -18,7 +18,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://www.casper.or.kr", "https://oauth2.googleapis.com", "https://www.googleapis.com", "https://github.com", "https://api.github.com")
+                .allowedOrigins("http://localhost:3000","https://www.casper.or.kr", "https://oauth2.googleapis.com", "https://www.googleapis.com", "https://github.com", "https://api.github.com")
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
