862815eaf92572f25cd8c271126da9b0841f8ab6
동헌희 <hui1601@naver.com>
2025-03-23 14:16:33 +0900

    fix: update resource handlers to use custom path resolver and improve URL decoding(#61)

--- src/main/java/com/example/newsper/config/WebMvcConfig.java
+++ src/main/java/com/example/newsper/config/WebMvcConfig.java
@@ -1,23 +1,39 @@
 package com.example.newsper.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
+@Slf4j
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String homePath = "/home/casper/";
         registry.addResourceHandler("/profile/**")
-                .addResourceLocations("file://" + homePath + "profile/");
+                .addResourceLocations("file://" + homePath + "profile/")
+                .resourceChain(true)
+                .addResolver(new CustomPathResourceResolver());
         registry.addResourceHandler("/article/**")
-                .addResourceLocations("file://" + homePath + "article/");
+                .addResourceLocations("file://" + homePath + "article/")
+                .resourceChain(true)
+                .addResolver(new CustomPathResourceResolver());
         registry.addResourceHandler("/assignment/**")
-                .addResourceLocations("file://" + homePath + "assignment/");
+                .addResourceLocations("file://" + homePath + "assignment/")
+                .resourceChain(true)
+                .addResolver(new CustomPathResourceResolver());
         registry.addResourceHandler("/submit/**")
-                .addResourceLocations("file://" + homePath + "submit/");
+                .addResourceLocations("file://" + homePath + "submit/")
+                .resourceChain(true)
+                .addResolver(new CustomPathResourceResolver());
     }
 
     @Override
@@ -27,5 +43,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(false);
+    }
+    static class CustomPathResourceResolver extends PathResourceResolver {
+        @SuppressWarnings("NullableProblems")
+        @Override
+        protected Resource getResource(String resourcePath, Resource location) throws IOException {
+            resourcePath = URLDecoder.decode(resourcePath, StandardCharsets.UTF_8);
+            return super.getResource(resourcePath, location);
+        }
     }
 }
